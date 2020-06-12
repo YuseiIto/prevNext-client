e8a147dd12b7249ea1155bab030a5b01716a84a7
Yusei Ito <yuseikun@Outlook.jp>
2020-06-13 02:09:55 +0900

    Basic websocket

--- main.py
+++ main.py
@@ -1,12 +1,31 @@
 import time
 import keyboard
+import websocket
+
+
+# Preferences
+path = 'ws://localhost:50000/'
+IS_DEBUG_MODE = False
+
+
 keyboard.press_and_release('shift+s, space')
 keyboard.write('The quick brown fox jumps over the lazy dog.')
 
+if IS_DEBUG_MODE:
+    websocket.enableTrace(True)
+
+ws = websocket.WebSocket()
+ws.connect(path)
+print("Sending 'Hello, World'...")
+ws.send("Hello, World")
+print("Sent")
+print("Receiving...")
+result = ws.recv()
+print("Received '%s'" % result)
+ws.close()
+
+""" 
 while True:
     keyboard.send('enter')
     print("Change LIne!")
-    time.sleep(0.8)
-
-# Block forever, like `while True`.
-keyboard.wait()
+    time.sleep(0.8) """
