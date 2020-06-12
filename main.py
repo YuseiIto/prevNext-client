846641d3b860d9967d468e97c7dd2d30845ba899
Yusei Ito <yuseikun@Outlook.jp>
2020-06-13 02:50:59 +0900

    Clean up the code

--- main.py
+++ main.py
@@ -2,13 +2,15 @@ import time
 import keyboard
 import websocket
 
-
 # Preferences
 path = 'ws://localhost:50000/'
 IS_DEBUG_MODE = False
 
 
-keyboard.write('The quick brown fox jumps over the lazy dog.')
+print("Remote-presentation-support")
+print("Created by Yusei Ito")
+print("- - - - - - - - - - ")
+print("Target server: " + path)
 
 if IS_DEBUG_MODE:
     websocket.enableTrace(True)
@@ -17,7 +19,9 @@ ws = websocket.WebSocket()
 ws.connect(path)
 ws.send("ready")
 
+print("\033[32mConnection established! Ready.\033[0m")
 
+# Main outine. continues fowever if conneced
 while ws.connected:
     message = ws.recv()
     if message == 'next':
@@ -27,5 +31,5 @@ while ws.connected:
         keyboard.send('left')
         print("prev")
 
-print("Websocket disconnected!")
+print("\033[92mWebsocket disconnected!\033[0m")
 ws.close()
